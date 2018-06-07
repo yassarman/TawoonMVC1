eca0e09c7ea41e35c835776bf032c778d399a548
yassar <yfman1234@gmail.com>
2018-06-07 13:07:21 +0300

    first commit

---
+++ TaawonMVC.Application/Buildings/DTO/GetBuildingsOutput.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaawonMVC.Buildings.DTO
+{
+  public  class GetBuildingsOutput
+    {
+        public int Id { get; set; }
+        public int buildingID { get; set; }
+        public int numOfBuildingUnits { get; set; }
+        public int numOfFloors { get; set; }
+        public string streetName { get; set; }
+        public int buildingNo { get; set; }
+        public int neighborhoodID { get; set; }
+        public int buildingTypeID { get; set; }
+        public string GISMAP { get; set; }
+        public byte houshProperty { get; set; }
+        public string houshName { get; set; }
+        public double X { get; set; }
+        public double Y { get; set; }
+    }
+}
---
+++ TaawonMVC.Web/App_Start/PageNames.cs
@@ -0,0 +1,13 @@
+namespace TaawonMVC.Web
+{
+    public class PageNames
+    {
+        public const string Home = "Home";
+        public const string About = "About";
+        public const string Tenants = "Tenants";
+        public const string Users = "Users";
+        public const string Roles = "Roles";
+        public const string BuildingsType = "BuildingsType";
+        public const string Neighborhood = "Neighborhood";
+    }
+}
